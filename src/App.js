fce8fcd87ee65a817d1d18382b0bc59fff5f2dbc
Tejas3157 <dungatejas77@gmail.com>
2025-10-20 22:55:25 +0530

    committ

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import Header from './components/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './components/Footer';
+import LandingPage from './components/Landingpage/LandingPage';
 
 function App() {
   return (
     <div className="app-layout d-flex flex-column min-vh-100">
         <Header />
+        <LandingPage />
         <div className="flex-grow-1">
           {/* Main content goes here */}
         </div>
